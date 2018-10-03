b3c142245d1a48388036fe1719740858c99123c8
Floglor <floglor033@gmail.com>
2018-10-03 17:37:33 +0300

    Added man's image and functionality to it.

--- app/src/main/java/ua/com/up_site/guiderenttest/MainActivity.java
+++ app/src/main/java/ua/com/up_site/guiderenttest/MainActivity.java
@@ -2,12 +2,32 @@ package ua.com.up_site.guiderenttest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
+
+    ImageView imageview;
+    private boolean isGirlOnImage = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageview = findViewById(R.id.imageView);
+
+        imageview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isGirlOnImage) {
+                    imageview.setImageResource(R.drawable.man_full);
+                    isGirlOnImage = false;
+                } else {
+                    imageview.setImageResource(R.drawable.girl);
+                    isGirlOnImage = true;
+                }
+            }
+        });
     }
 }
