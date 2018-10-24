35a0e6c5d0b85592fa710fa0540a028368c02a05
dashaboichuk <darusia97@ukr.net>
2018-10-24 15:47:47 +0300

    Fixed Networks Activity.

--- app/src/main/java/ua/com/up_site/guiderenttest/login_page/NetworksActivity.java
+++ app/src/main/java/ua/com/up_site/guiderenttest/login_page/NetworksActivity.java
@@ -3,6 +3,8 @@ package ua.com.up_site.guiderenttest.login_page;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
@@ -14,6 +16,7 @@ import com.facebook.login.widget.LoginButton;
 
 import java.util.Arrays;
 
+import ua.com.up_site.guiderenttest.MainActivity;
 import ua.com.up_site.guiderenttest.R;
 
 public class NetworksActivity extends AppCompatActivity {
@@ -36,7 +39,8 @@ public class NetworksActivity extends AppCompatActivity {
                 new FacebookCallback<LoginResult>() {
                     @Override
                     public void onSuccess(LoginResult loginResult) {
-                        // App code
+
+
                     }
 
                     @Override
@@ -49,15 +53,20 @@ public class NetworksActivity extends AppCompatActivity {
                         // App code
                     }
                 });
+
+        //AccessToken.getCurrentAccessToken можно загрузить вместе с SDK из кэша или из закладки приложения при холодном запуске. Проверить его действительность можно в методе onCreateActivity:
         AccessToken accessToken = AccessToken.getCurrentAccessToken();
         boolean isLoggedIn = accessToken != null && !accessToken.isExpired();
 
-        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile"));
+        //вход, например с помощью элемента OnClickListener индивидуально настроенной кнопки:
+      //  LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("public_profile"));
 
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         callbackManager.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
+        Intent activityChangeIntent = new Intent(NetworksActivity.this, MainActivity.class);
+        NetworksActivity.this.startActivity(activityChangeIntent);
     }
 }
